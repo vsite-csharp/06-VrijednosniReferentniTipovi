3a2908544f9de4ed2287ff398710f854363ba628
Roland Rostohar <rorostoh@vsite.hr>
2019-11-21 20:27:12 +0100

    Resolve boxing

--- Zapakiravanje/Zapakiravanje.cs
+++ Zapakiravanje/Zapakiravanje.cs
@@ -27,9 +27,13 @@ namespace Vsite.CSharp.VrijednosniReferentniTip
                 lista.Add(rt);
             }
 
-            // TODO:010 Dodati petlju koja će za svaki član liste ispisati vrijednost njegovog člana X
-
-        }
+			
+			for (int i = 0; i < 10; ++i)
+			{
+				RefTip r = (RefTip) lista[i];
+				Console.WriteLine(r.X);
+			}
+		}
 
         public static void DodavanjeStruktureUArrayList()
         {
@@ -42,9 +46,13 @@ namespace Vsite.CSharp.VrijednosniReferentniTip
                 lista.Add(vt);
             }
 
-            // TODO:011 Dodati petlju koja će za svaki član liste ispisati vrijednost njegovog člana X
-
-        }
+			
+			for (int i = 0; i < 10; ++i)
+			{
+				VrijednTip r = (VrijednTip)lista[i];
+				Console.WriteLine(r.X);
+			}
+		}
 
 
         static void Main(string[] args)
