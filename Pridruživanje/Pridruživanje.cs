cf4f3107da7535fde818cd64c6fb98630bd3dad7
Roland Rostohar <rorostoh@vsite.hr>
2019-11-21 20:17:19 +0100

    Resolved assigments

--- Pridruživanje/Pridruživanje.cs
+++ Pridruživanje/Pridruživanje.cs
@@ -22,67 +22,70 @@ namespace Vsite.CSharp.VrijednosniReferentniTip
             VrijednTip vt1 = new VrijednTip();
             vt1.X = prvaVrijednost;
 
-            // TODO:000 Deklarirati novi objekt "vt2" tipa VrijednTip i pridružiti mu vrijednost objekta "vt1"
+			VrijednTip vt2 = vt1;
 
+			Console.WriteLine(vt1.X);
+			Console.WriteLine(vt2.X);
 
-            // TODO:001 Dodati dvije naredbe koje će ispisati vrijednosti članova vt1.X, odnosno vt2.X
-
-
-            Console.WriteLine($"Pridružujemo vt1.X={drugaVrijednost}:");
+			Console.WriteLine($"Pridružujemo vt1.X={drugaVrijednost}:");
             vt1.X = drugaVrijednost;
 
-            // TODO:002 Dodati dvije naredbe koje će ponovno ispisati vrijednosti članova vt1.X, odnosno vt2.X. Pokrenuti program i provjeriti ispis
+			Console.WriteLine(vt1.X);
+			Console.WriteLine(vt2.X);
 
-        }
+		}
 
-        public static void PridruživanjeKlasa(int prvaVrijednost, int drugaVrijednost)
+		public static void PridruživanjeKlasa(int prvaVrijednost, int drugaVrijednost)
         {
             RefTip rt1 = new RefTip();
             rt1.X = prvaVrijednost;
 
-            // TODO:003 Deklarirati novi objekt "rt2" RefTip i pridružiti mu objekt "rt1"
+			RefTip rt2 = rt1;
+
+			Console.WriteLine(rt1.X);
+			Console.WriteLine(rt2.X);
 
 
-            // TODO:004 Dodati dvije naredbe koje će ispisati vrijednosti članova rt1.X, odnosno rt2.X
-
-
-            Console.WriteLine($"Pridružujemo rt1.X={drugaVrijednost}:");
+			Console.WriteLine($"Pridružujemo rt1.X={drugaVrijednost}:");
             rt1.X = drugaVrijednost;
 
-            // TODO:005 Dodati dvije naredbe koje će ponovno ispisati vrijednosti članova rt1.X, odnosno rt2.X. Pokrenuti program i provjeriti ispis
+			Console.WriteLine(rt1.X);
+			Console.WriteLine(rt2.X);
 
-        }
+		}
 
-        public static void PridruživanjeNovogObjektaČlanuPreslikaneStukture(int prvaVrijednost, int drugaVrijednost)
+		public static void PridruživanjeNovogObjektaČlanuPreslikaneStukture(int prvaVrijednost, int drugaVrijednost)
         {
             VrijednTip vt1 = new VrijednTip();
             vt1.X = prvaVrijednost;
             vt1.Rt = new RefTip();
             vt1.Rt.X = prvaVrijednost;
 
-            // TODO:006 Deklarirati novi objekt "vt2" tipa VrijednTip i pridružiti mu objekt "vt1"
+			VrijednTip vt2 = vt1;
+
+			Console.WriteLine(vt1.Rt.X);
+			Console.WriteLine(vt2.Rt.X);
 
 
-            // TODO:007 Dodati naredbe koje će ispisati vrijednosti članova vt1.Rt.X, odnosno vt2.Rt.X
-
-
-            Console.WriteLine($"Pridružujemo vt1.Rt.X={drugaVrijednost}:");
+			Console.WriteLine($"Pridružujemo vt1.Rt.X={drugaVrijednost}:");
             vt1.Rt.X = drugaVrijednost;
 
-            // TODO:008 Dodati dvije naredbe koje će ponovno ispisati vrijednosti članova vt1.Rt.X, odnosno vt2.Rt.X. Pokrenuti program i provjeriti ispis
+			Console.WriteLine(vt1.Rt.X);
+			Console.WriteLine(vt2.Rt.X);
 
 
-            Console.WriteLine($"Članu vt1.Rt pridružujemo novi objekt i zadajemo da je vt1.Rt.X={prvaVrijednost}:");
+			Console.WriteLine($"Članu vt1.Rt pridružujemo novi objekt i zadajemo da je vt1.Rt.X={prvaVrijednost}:");
 
-            // TODO:009 Otkomentirati donje naredbe, ponoviti naredbe za ispis vrijednosti članova vt1.Rt.X, odnosno vt2.Rt.X. Pokrenuti program i provjeriti ispis
-            //vt1.Rt = new RefTip();
-            //vt1.Rt.X = prvaVrijednost;
+            vt1.Rt = new RefTip();
+            vt1.Rt.X = prvaVrijednost;
+
+			Console.WriteLine(vt1.Rt.X);
+			Console.WriteLine(vt2.Rt.X);
+
+		}
 
 
-        }
-
-
-        static void Main(string[] args)
+		static void Main(string[] args)
         {
             Console.WriteLine("Pridruživanje vrijednosnih tipova (struktura)");
             PridruživanjeStruktura(1, 5);
