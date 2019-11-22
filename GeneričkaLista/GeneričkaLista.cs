5e5c7b9c26428eb27e256396b3ccbcd22c49509c
Krunoslav Bučar <krbuki@gmail.com>
2019-11-22 21:38:04 +0100

    Resolve Generic List

--- GeneričkaLista/GeneričkaLista.cs
+++ GeneričkaLista/GeneričkaLista.cs
@@ -29,7 +29,10 @@ namespace Vsite.CSharp.VrijednosniReferentniTip
         public static void Ispis(List<VrijednTip> lista)
         {
             // TODO:030 Dodati petlju koja će ispisati član X za svaki element kolekcije lista.
-
+            foreach(VrijednTip element in lista)
+            {
+                Console.WriteLine(element.X);
+            }
             // TODO:031 Prevesti kod i usporediti dobiveni IL kod s kodom iz projekta Otpakiravanje.
 
         }
