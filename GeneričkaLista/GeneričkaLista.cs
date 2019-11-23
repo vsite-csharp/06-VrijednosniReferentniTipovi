9560a45c1ca972d7355fc47796049569837fc568
Auro Mazzi <auro.mazzi@gmail.com>
2019-11-23 16:56:08 +0100

    Resolve generic list

--- GeneričkaLista/GeneričkaLista.cs
+++ GeneričkaLista/GeneričkaLista.cs
@@ -29,7 +29,10 @@ namespace Vsite.CSharp.VrijednosniReferentniTip
         public static void Ispis(List<VrijednTip> lista)
         {
             // TODO:030 Dodati petlju koja će ispisati član X za svaki element kolekcije lista.
-
+            foreach (VrijednTip vt in lista)
+            {
+                Console.WriteLine(vt.X);
+            }
             // TODO:031 Prevesti kod i usporediti dobiveni IL kod s kodom iz projekta Otpakiravanje.
 
         }
