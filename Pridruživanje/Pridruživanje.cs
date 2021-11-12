cb2da487fc3c27c88c412a0c0a2b70b32d9e3cda
Damir Stojanović <DStojanovic@dspace.hr>
2021-11-12 11:35:07 +0100

    Vrijednosni referentni tipovi

--- Pridruživanje/Pridruživanje.cs
+++ Pridruživanje/Pridruživanje.cs
@@ -22,17 +22,17 @@ namespace Vsite.CSharp.VrijednosniReferentniTip
             VrijednTip vt1 = new VrijednTip();
             vt1.X = prvaVrijednost;
 
-            // TODO:000 Deklarirati novi objekt "vt2" tipa VrijednTip i pridružiti mu vrijednost objekta "vt1"
+            // 000 Deklarirati novi objekt "vt2" tipa VrijednTip i pridružiti mu vrijednost objekta "vt1"
             VrijednTip vt2 = vt1;
 
-            // TODO:001 Dodati naredbe koje će ispisati vrijednosti članova vt1.X, odnosno vt2.X
+            // :001 Dodati naredbe koje će ispisati vrijednosti članova vt1.X, odnosno vt2.X
             Console.WriteLine(vt1.X);
             Console.WriteLine(vt2.X);
 
             Console.WriteLine($"Pridružujemo vt1.X={drugaVrijednost}:");
             vt1.X = drugaVrijednost;
 
-            // TODO:002 Ponoviti naredbe za ispis vrijednosti članova vt1.X, odnosno vt2.X. Pokrenuti program i provjeriti ispis
+            // :002 Ponoviti naredbe za ispis vrijednosti članova vt1.X, odnosno vt2.X. Pokrenuti program i provjeriti ispis
             Console.WriteLine(vt1.X);
             Console.WriteLine(vt2.X);
         }
@@ -42,17 +42,17 @@ namespace Vsite.CSharp.VrijednosniReferentniTip
             RefTip rt1 = new RefTip();
             rt1.X = prvaVrijednost;
 
-            // TODO:003 Deklarirati novi objekt "rt2" RefTip i pridružiti mu objekt "rt1"
+            // :003 Deklarirati novi objekt "rt2" RefTip i pridružiti mu objekt "rt1"
             RefTip rt2 = rt1;
 
-            // TODO:004 Dodati naredbe koje će ispisati vrijednosti članova rt1.X, odnosno rt2.X
+            // :004 Dodati naredbe koje će ispisati vrijednosti članova rt1.X, odnosno rt2.X
             Console.WriteLine(rt1.X);
             Console.WriteLine(rt2.X);
 
             Console.WriteLine($"Pridružujemo rt1.X={drugaVrijednost}:");
             rt1.X = drugaVrijednost;
 
-            // TODO:005 Ponoviti naredbe za ispis vrijednosti članova rt1.X, odnosno rt2.X. Pokrenuti program i provjeriti ispis
+            // :005 Ponoviti naredbe za ispis vrijednosti članova rt1.X, odnosno rt2.X. Pokrenuti program i provjeriti ispis
             Console.WriteLine(rt1.X);
             Console.WriteLine(rt2.X);
         }
@@ -64,7 +64,7 @@ namespace Vsite.CSharp.VrijednosniReferentniTip
             vt1.Rt = new RefTip();
             vt1.Rt.X = prvaVrijednost;
 
-            // TODO:006 Deklarirati novi objekt "vt2" tipa VrijednTip i pridružiti mu objekt "vt1"
+            // :006 Deklarirati novi objekt "vt2" tipa VrijednTip i pridružiti mu objekt "vt1"
             var vt2 = vt1;
             
             Console.WriteLine(vt1.Rt.X);
@@ -78,14 +78,14 @@ namespace Vsite.CSharp.VrijednosniReferentniTip
             Console.WriteLine($"Pridružujemo vt1.Rt.X={drugaVrijednost}:");
             vt1.Rt.X = drugaVrijednost;
 
-            // TODO:008 Ponoviti naredbe za ispis vrijednosti članova vt1.Rt.X, odnosno vt2.Rt.X. Pokrenuti program i provjeriti ispis
+            // :008 Ponoviti naredbe za ispis vrijednosti članova vt1.Rt.X, odnosno vt2.Rt.X. Pokrenuti program i provjeriti ispis
 
             Console.WriteLine(vt1.Rt.X);
             Console.WriteLine(vt2.Rt.X);
 
             Console.WriteLine($"Članu vt1.Rt pridružujemo novi objekt i zadajemo da je vt1.Rt.X={prvaVrijednost}:");
 
-            // TODO:009 Otkomentirati donje naredbe, ponoviti naredbe za ispis vrijednosti članova vt1.Rt.X, odnosno vt2.Rt.X. Pokrenuti program i provjeriti ispis.
+            // :009 Otkomentirati donje naredbe, ponoviti naredbe za ispis vrijednosti članova vt1.Rt.X, odnosno vt2.Rt.X. Pokrenuti program i provjeriti ispis.
             vt1.Rt = new RefTip();
             vt1.Rt.X = prvaVrijednost;
 
@@ -94,7 +94,7 @@ namespace Vsite.CSharp.VrijednosniReferentniTip
 
 
         }
-        // TODO:009a Pokrenuti testove i provjeriti prolaze li sva 3 testa u grupi TestPridruživanja.
+        // :009a Pokrenuti testove i provjeriti prolaze li sva 3 testa u grupi TestPridruživanja.
 
         static void Main(string[] args)
         {
