32042176b123ca94445d7df3360517d5a5350fa4
Andrea Vrljic <andrea31542@gmail.com>
2021-11-12 17:58:14 +0100

    Resolved Zapakiravanje

--- Zapakiravanje/Zapakiravanje.cs
+++ Zapakiravanje/Zapakiravanje.cs
@@ -27,7 +27,12 @@ namespace Vsite.CSharp.VrijednosniReferentniTip
                 lista.Add(rt);
             }
 
-            // TODO:020 Dodati petlju koja će za svaki član liste ispisati vrijednost njegovog člana X
+            // :020 Dodati petlju koja će za svaki član liste ispisati vrijednost njegovog člana X
+
+            foreach (var item in lista)
+            {
+                Console.WriteLine((item as RefTip)?.X);
+            }
 
         }
 
@@ -42,10 +47,13 @@ namespace Vsite.CSharp.VrijednosniReferentniTip
                 lista.Add(vt);
             }
 
-            // TODO:021 Dodati petlju koja će za svaki član liste ispisati vrijednost njegovog člana X
-
+            // :021 Dodati petlju koja će za svaki član liste ispisati vrijednost njegovog člana X
+            foreach (var item in lista)
+            {
+                Console.WriteLine(((VrijednTip) item).X);
+            }
         }
-        // TODO:022 Pokrenuti testove i provjeriti prolaze li oba testa u grupi TestZapakiravanja
+        // :022 Pokrenuti testove i provjeriti prolaze li oba testa u grupi TestZapakiravanja
 
         static void Main(string[] args)
         {
