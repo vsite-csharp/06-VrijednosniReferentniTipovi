76e1c6e5701177fa4c9ed153ce71e2235cedd798
Marina Letica <MLetica@dspace.hr>
2021-11-20 23:04:17 +0100

    GC impact: removed TODO

--- UtjecajGCa/UtjecajGCa.cs
+++ UtjecajGCa/UtjecajGCa.cs
@@ -27,7 +27,7 @@ namespace Vsite.CSharp.VrijednosniReferentniTip
     {
         static void Main(string[] args)
         {
-            // TODO:010 Pogledati što kod radi, pokrenuti program i analizirati ispisana vremena.
+            // :010 Pogledati što kod radi, pokrenuti program i analizirati ispisana vremena.
             const int brojElemenata = 1000000;
 
             // stvaramo objekte i pokrećemo metode da bi JIT preveo u strojni kod prije korištenja
