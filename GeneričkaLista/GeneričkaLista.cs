bef740d189d9118ea97906beade538d2f97d7bcf
Marko Bohač <mabohac@vsite.hr>
2022-12-01 17:35:58 +0100

    Resolved generic list

--- GeneričkaLista/GeneričkaLista.cs
+++ GeneričkaLista/GeneričkaLista.cs
@@ -3,36 +3,39 @@ using System.Collections.Generic;
 
 namespace Vsite.CSharp.VrijednosniReferentniTip
 {
-    class GeneričkaLista
-    {
-        public struct VrijednTip
-        {
-            public int X;
-        }
+	class GeneričkaLista
+	{
+		public struct VrijednTip
+		{
+			public int X;
+		}
 
-        static void Main(string[] args)
-        {
-            List<VrijednTip> lista = new List<VrijednTip>();
-            VrijednTip vt = new VrijednTip();
-            for (int i = 0; i < 10; ++i)
-            {
-                vt.X = i;
-                lista.Add(vt);
-            }
+		static void Main(string[] args)
+		{
+			List<VrijednTip> lista = new List<VrijednTip>();
+			VrijednTip vt = new VrijednTip();
+			for (int i = 0; i < 10; ++i)
+			{
+				vt.X = i;
+				lista.Add(vt);
+			}
 
-            Ispis(lista);
+			Ispis(lista);
 
-            Console.WriteLine("GOTOVO!!!");
-            Console.ReadKey();
-        }
+			Console.WriteLine("GOTOVO!!!");
+			Console.ReadKey();
+		}
 
-        public static void Ispis(List<VrijednTip> lista)
-        {
-            // TODO:060 Dodati petlju koja će ispisati član X za svaki element kolekcije lista.
+		public static void Ispis(List<VrijednTip> lista)
+		{
+			// 060 Dodati petlju koja će ispisati član X za svaki element kolekcije lista.
+			foreach (var item in lista)
+			{
+				Console.WriteLine(item.X);
+			}
 
-            // TODO:061 Prevesti kod i usporediti dobiveni IL kod s kodom iz projekta Otpakiravanje.
-
-            // TODO:062 Pokrenuti testove i provjeriti prolazi li test u grupi TestGeneričkeListe.
-        }
-    }
+			// 061 Prevesti kod i usporediti dobiveni IL kod s kodom iz projekta Otpakiravanje.
+			// 062 Pokrenuti testove i provjeriti prolazi li test u grupi TestGeneričkeListe.
+		}
+	}
 }
